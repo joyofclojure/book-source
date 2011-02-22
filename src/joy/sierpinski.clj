3685763d9ed1e27fb14da6fc65ad1f999bfcffe5
fogus <mefogus@gmail.com>
2011-02-22 15:14:30 -0500

    Added Sierpinski square example from ch3.

---
+++ src/joy/sierpinski.clj
@@ -0,0 +1,34 @@
+(ns joy.sierpinski
+  "Sierpinski squares example from section 3.4")
+
+(defn xors [xs ys]
+  (for [x (range xs) y (range ys)]
+    [x y (rem (bit-xor x y) 256)]))
+
+(def frame (java.awt.Frame.))
+
+(.setVisible frame true)
+(.setSize frame (java.awt.Dimension. 200 200))
+
+(def gfx (.getGraphics frame))
+
+(.fillRect gfx 100 100 50 75)
+(.setColor gfx (java.awt.Color. 255 128 0))
+(.fillRect gfx 100 150 75 50)
+
+(defn clear [g] (.clearRect g 0 0 200 200))
+
+(defn f-values [f xs ys]
+  (for [x (range xs) y (range ys)]
+    [x y (rem (f x y) 256)]))
+
+(defn draw-values [f xs ys]
+  (clear gfx)
+  (.setSize frame (java.awt.Dimension. xs ys))
+  (doseq [[x y v] (f-values f xs ys)]
+    (.setColor gfx (java.awt.Color. v v v))
+    (.fillRect gfx x y 1 1)))
+
+(draw-values bit-and 256 256)
+(draw-values + 256 256)
+(draw-values * 256 256)
