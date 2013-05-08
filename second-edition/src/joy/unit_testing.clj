f317340a6c5f537c50c89d2a53c6e372540f6bab
fogus <mefogus@gmail.com>
2013-05-08 15:07:09 -0400

    Ready for unit testing discussion

--- second-edition/src/joy/unit_testing.clj
+++ second-edition/src/joy/unit_testing.clj
@@ -3,3 +3,19 @@
 
 (def stubbed-feed-children
   (constantly [{:tag :title :content ["Stub"]}]))
+
+(defn count-feed-entries [url]
+  (count (feed-children url)))
+
+(comment
+  (count-feed-entries "http://blog.fogus.me/feed/")
+  
+  ;;=> 5
+
+  (with-redefs [feed-children stubbed-feed-children]
+    (count-feed-entries "http://blog.fogus.me/feed/"))
+
+  ;;=> 1
+)
+
+
