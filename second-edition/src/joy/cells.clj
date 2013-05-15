58c5ed70ac814fd3394ad1f59b59083a67644722
fogus <mefogus@gmail.com>
2013-05-15 07:34:09 -0400

    Working on cells examples

--- second-edition/src/joy/cells.clj
+++ second-edition/src/joy/cells.clj
@@ -2,11 +2,11 @@
 
 (defmacro defformula [nm bindings & formula]
   `(let ~bindings
-     (let [formula#   (agent ~@formula) ;; #: Create formula as Agent
+     (let [formula#   (agent ~@formula) 
            update-fn# (fn [key# ref# o# n#] 
                         (send formula# (fn [_#] ~@formula)))]
        (doseq [r# ~(vec (map bindings (range 0 (count bindings) 2)))]
-         (add-watch r# :update-formula update-fn#)) ;; #: Add a watch to each reference
+         (add-watch r# :update-formula update-fn#)) 
        (def ~nm formula#))))
     
 (def h (ref 25))
