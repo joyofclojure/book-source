262476abdb3c6bd40fedc710c3cfdc547ae826a5
Chouser <chouser@lonocloud.com>
2013-06-08 13:16:13 -0400

    Update refs for 2nd edition

--- second-edition/src/joy/refs.clj
+++ second-edition/src/joy/refs.clj
@@ -3,7 +3,6 @@
   (use [joy.mutation :only (dothreads!)]
        [joy.a :only (neighbors)]))
 
-
 (def initial-board
   [[:- :k :-]
    [:- :- :-]
@@ -20,13 +19,27 @@
   (def to-move (ref [[:K [2 1]] [:k [0 1]]]))
   (def num-moves (ref 0)))
 
-(def king-moves (partial neighbors [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]] 3))
+(def king-moves
+  (partial neighbors
+           [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]] 3))
 
-(defn good-move? [to enemy-sq] (when (not= to enemy-sq) to))
+(defn good-move?
+  "Returns 'to' position if it isn't already the 'enemy-sq' position."
+  [to enemy-sq]
+  (when (not= to enemy-sq)
+    to))
 
-(defn choose-move [[[mover mpos][_ enemy-pos]]]
+(defn choose-move
+  "Randomly choose a legal move"
+  [[[mover mpos][_ enemy-pos]]]
   [mover (some #(good-move? % enemy-pos)
                (shuffle (king-moves mpos)))])
+
+(comment
+  (reset-board!)
+  (take 5 (repeatedly #(choose-move @to-move)))
+  ;=> ([:K [1 0]] [:K [1 2]] [:K [1 1]] [:K [1 2]] [:K [1 0]])
+  )
 
 (defn place [from to] to)
 
@@ -38,19 +51,53 @@
 (defn update-to-move [move]
   (alter to-move #(vector (second %) move)))
 
-(defn make-move []
+(defn make-move []  ;; bad!
+  (let [move (choose-move @to-move)]
+    (dosync (move-piece move @to-move))
+    (dosync (update-to-move move))))
+
+(comment
+  (reset-board!)
+
+  (make-move)
+  ;=> [[:k [0 1]] [:K [2 0]]]
+  (board-map deref board)
+  ;=> [[:- :k :-] [:- :- :-] [:K :- :-]]
+
+  (make-move)
+  ;=> [[:K [2 0]] [:k [1 1]]]
+  (board-map deref board)
+  ;=> [[:- :- :-] [:- :k :-] [:K :- :-]]
+
+  (dothreads! make-move :threads 100 :times 100)
+  (board-map deref board)
+  ;=> [[:- :- :-] [:- :K :-] [:- :- :K]]</programlisting></informalexample>  ;; oops!
+  )
+
+(defn make-move-v2 [] ;; good!
   (dosync
    (let [move (choose-move @to-move)]
      (move-piece move @to-move)
      (update-to-move move))))
 
-(defn go [move-fn threads times]
-  (dothreads! move-fn :threads threads :times times))
+(comment
+    (reset-board!)
+    (make-move)
+    ;=> [[:k [0 1]] [:K [2 0]]]
+    (board-map deref board)
+    ;=> [[:- :k :-] [:- :- :-] [:K :- :-]]
+    @num-moves
+    ;=> 1
 
-(defn bad-make-move []
-  (let [move (choose-move @to-move)]
-    (dosync (move-piece move @to-move))
-    (dosync (update-to-move move))))
+    (dothreads! make-move-v2 :threads 100 :times 100)
+    (board-map #(dosync (deref %)) board)
+    ;=> [[:k :- :-] [:- :- :-] [:K :- :-]]
+    @to-move
+    ;=> [[:k [0 0]] [:K [2 0]]]
+    @num-moves
+    ;=> 10001
+    )
+
 
 (defn move-piece [[piece dest] [[_ src] _]]
   (commute (get-in board dest) place piece)
