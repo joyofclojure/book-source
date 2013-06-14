8864088f983f048ffcf91222a28a1091fb445326
fogus <mefogus@gmail.com>
2013-06-14 08:56:21 -0400

    Added data-centric code

--- second-edition/src/joy/generators.clj
+++ second-edition/src/joy/generators.clj
@@ -1,1 +1,22 @@
 (ns joy.generators)
+
+(def ascii (map char (range 65 (+ 65 26))))
+
+(defn rand-str [sz alphabet]
+  (apply str (repeatedly sz #(rand-nth alphabet))))
+
+(def rand-sym #(symbol (rand-str %1 %2)))
+
+(def rand-key #(keyword (rand-str %1 %2)))
+
+(comment
+
+  (rand-str 10 ascii)
+
+  (rand-key 10 ascii)
+
+  (rand-sym 10 ascii)
+  
+)
+
+
