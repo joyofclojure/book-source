591b3e85f7f2229eb5616a86e6caafd85f21a2fe
fogus <mefogus@gmail.com>
2013-06-14 08:58:38 -0400

    Added rand-map

--- second-edition/src/joy/generators.clj
+++ second-edition/src/joy/generators.clj
@@ -23,11 +23,17 @@
 (defn rand-vec [& generators]
   (into [] (map #(%) generators)))
 
+(defn rand-map [sz kgen vgen]
+  (into {}
+        (repeatedly sz #(rand-vec kgen vgen))))
+
 (comment
 
   (rand-vec #(rand-sym 5 ascii)
             #(rand-key 10 ascii)
             #(rand-int 1024))
 
+
+  (rand-map 3 #(rand-key 5 ascii) #(rand-int 100))
 )
 
