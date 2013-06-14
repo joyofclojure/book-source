5a41901fada20c41142545a73198c3825f806b65
fogus <mefogus@gmail.com>
2013-06-14 13:54:36 -0400

    Sim testing update-stats

--- second-edition/src/joy/sim_test.clj
+++ second-edition/src/joy/sim_test.clj
@@ -1,15 +1,29 @@
 (ns joy.sim-test
   (:require [joy.event-sourcing :as es]
             [joy.generators :refer (rand-map)]
-            [clojure.set :as set]))
+            [clojure.set :as sql]))
 
 
 (def db (ref #{{:player "Nick", :ability 32}
                {:player "Matt", :ability 26}
                {:player "Ryan", :ability 19}}))
 
+(defn update-stats [db event]
+  (let [player (first (sql/select #(= (:player event)
+                                      (:player %))
+                                  db))
+        less-db  (sql/difference db #{player})]
+    (conj less-db (merge player (es/effect player event)))))
 
+(comment
 
+  (sql/select #(= "Nick" (:player %)) @db)
+
+  ;;=> #{{:ability 32, :player "Nick"}}
+
+  (update-stats @db {:player "Nick", :result :hit})
+
+)
 
 (defn rand-event [max ability]
   (rand-map 1
