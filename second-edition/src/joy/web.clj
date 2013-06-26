0194469b529a9dab5c6560a9e802f3324aac6d5d
fogus <mefogus@gmail.com>
2013-06-25 22:40:35 -0400

    respond

--- second-edition/src/joy/web.clj
+++ second-edition/src/joy/web.clj
@@ -29,6 +29,15 @@
     (.start)))
 
 
+(comment
+  (def server (new-server 8123 "/joy/hello" (default-handler "Hello Cleveland")))
+
+  (.stop server 0)
+  (def p (default-handler "There's no problem that can't be solved with another level of indirection"))
+  (def server (new-server 8123 "/joy/hello" p))
+)
+
+
 (defn echo-handler [pickler]
   (proxy [HttpHandler] []
     (handle [exchange]
