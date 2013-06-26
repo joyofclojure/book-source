ce527af3e39da801749688fa79c76a0225ef4ac6
fogus <mefogus@gmail.com>
2013-06-25 22:11:04 -0400

    echo-handler

--- second-edition/src/joy/web.clj
+++ second-edition/src/joy/web.clj
@@ -1,10 +1,19 @@
 (ns joy.web
   (:require [clojure.java.io :as io])
   (:import [java.io File]
-           [com.sun.net.httpserver HttpHandler HttpExchange HttpServer]
+           [com.sun.net.httpserver HttpHandler
+            HttpExchange HttpServer]
            [java.net InetSocketAddress HttpURLConnection]
            [java.io IOException FilterOutputStream
             BufferedInputStream FileInputStream]
            [java.net URLDecoder]))
     
+(defn echo-handler [pickler]
+  (proxy [HttpHandler] []
+    (handle [exchange]
+      (let [headers (pickler (.getRequestHeaders exchange))]
+        (.add (.getResponseHeaders exchange)
+              "Content-Type" "application/edn")
+        (respond exchange (prn-str headers))))))
+
 
