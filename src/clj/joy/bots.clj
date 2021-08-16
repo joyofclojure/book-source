2803fcd1da2daf493d19263d9c806736d45bb785
Chouser <chouser@n01se.net>
2021-08-15 22:19:46 -0400

    Fix turns to prevent negative bearings

--- src/clj/joy/bots.clj
+++ src/clj/joy/bots.clj
@@ -18,8 +18,15 @@
    :forward    (fn [] (bot (+ x (:x (bearings bearing-num)))
                            (+ y (:y (bearings bearing-num)))
                            bearing-num))
-   :turn-right (fn [] (bot x y (rem (+ 1 bearing-num) 4)))
-   :turn-left  (fn [] (bot x y (rem (+ 3 bearing-num) 4)))})
+   :turn-right (fn [] (bot x y (mod (+ 1 bearing-num) 4)))
+   :turn-left  (fn [] (bot x y (mod (+ 3 bearing-num) 4)))})
+
+(assert (= :west (:bearing ((:turn-left  (bot 0 0 0))))))
+(assert (= :east (:bearing ((:turn-right (bot 0 0 0))))))
+(assert (= :east
+           (:bearing ((:forward ((:forward ((:turn-right (bot 5 5 0))))))))))
+(assert (= [7 5]
+           (:coords ((:forward ((:forward ((:turn-right (bot 5 5 0))))))))))
 
 
 (defn mirror-bot [x y bearing-num]
@@ -28,6 +35,8 @@
    :forward    (fn [] (mirror-bot (- x (:x (bearings bearing-num)))
                                   (- y (:y (bearings bearing-num)))
                                   bearing-num))
-   :turn-right (fn [] (mirror-bot x y (rem (+ 3 bearing-num) 4)))
-   :turn-left  (fn [] (mirror-bot x y (rem (+ 1 bearing-num) 4)))})
+   :turn-right (fn [] (mirror-bot x y (mod (+ 3 bearing-num) 4)))
+   :turn-left  (fn [] (mirror-bot x y (mod (+ 1 bearing-num) 4)))})
 
+(assert (= :east (:bearing ((:turn-left  (mirror-bot 0 0 0))))))
+(assert (= :west (:bearing ((:turn-right (mirror-bot 0 0 0))))))
