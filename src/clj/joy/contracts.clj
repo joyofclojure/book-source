f463afa20f0fa51549a7638c9dfdeb54b2b758d3
Chouser <chouser@n01se.net>
2021-08-15 22:20:32 -0400

    Add a build-contract using auto-gensym to avoid name capture

--- src/clj/joy/contracts.clj
+++ src/clj/joy/contracts.clj
@@ -35,6 +35,33 @@
                     :else (throw (Exception. (str "Unknown tag " (first con)))))))
      (list* 'f args)))) ;; #6_build-contract: Build call to f
 
+;; The above version of build-contract which appears in editions 1 and 2
+;; captures the name "f":
+
+(assert
+ (= :name-captured
+    (try
+     ((defcontract oops [f] (require (number? f)) (ensure f))
+      (constantly :ok)
+      10)
+     (catch ClassCastException ex
+       :name-captured))))
+
+;; Here is an alternate version of build-contract that uses auto-gensym to avoid
+;; name capture:
+
+(defn build-contract [c]
+  (let [args (first c)]    ;; #1_build-contract: Grab args
+    `([f# ~@args]          ;; #3_build-contract: Include HOF + args
+      ~(apply merge
+              (for [con (rest c)]
+                (cond (= (first con) 'require) ;; #4_build-contract: Process "requires"
+                      (assoc {} :pre (vec (rest con)))
+                      (= (first con) 'ensure) ;; #5_build-contract: Process "ensures"
+                      (assoc {} :post (vec (rest con)))
+                      :else (throw (Exception. (str "Unknown tag " (first con)))))))
+      (f# ~@args)))) ;; #6_build-contract: Build call to f
+
 (def doubler-contract ;; #1_contract_comp: Define a contract
   (defcontract doubler 
     [x]
@@ -46,3 +73,8 @@
     
 (def times2 (partial doubler-contract #(* 2 %))) ;; #2_contract_comp: Test correct fn
 
+(assert
+ (= :ok
+    ((defcontract oops [f] (require (number? f)) (ensure f))
+     (constantly :ok)
+     10)))
